1b547dff786d30d803e9e73c1f5ed6edf4e06c87
Soumya <mailsourav123@gmail.com>
2018-04-20 11:46:36 +0530

    Prevalidatorv1.0

--- Pre_validator.py
+++ Pre_validator.py
@@ -1,13 +1,50 @@
 #check config file existence
-import os
+import os,sys
 from pathlib import Path
+import numpy as np
+import cv2 as cv
+import socket
+import tempfile
+import errno
 config_file_location = Path("E:\\Img_reckognition_Config\\config.mlt")
 if config_file_location.is_file() :
     print ("File Exists")
 else:
     print ("Config File Not existing !!!")
-#check mandatory values in config file
-
-#check internet connection
-#check camera
 #check write access in the pic directory
+path = "C:\\Program Files (x86)"
+print ("file mode {}" .format(os.access(path, os.W_OK)))
+#check internet connection
+try:
+    socket.create_connection(("www.google.com", 80))
+    print ("True")
+except OSError:
+    print ("False")
+#check camera
+def camera_test():
+    try:
+        cap = cv.VideoCapture(0)
+        if cap.isOpened():
+            print("Webcam online.")
+        cap.release()
+        cv.destroyAllWindows()
+    except:
+        print ("Either you don't have camera or there was some error trying to use camera")
+#check mandatory values in config file
+with open("E:\\Img_reckognition_Config\\config.mlt","r") as reading_config:
+    #data = reading_config.readline()
+    #print (data)
+    data_dict = dict()
+    while(True):
+        data = reading_config.readline()
+        if data != "":
+            key_val = data.split("=")
+            if key_val[1] not in ['',None,"",'""']:
+                data_dict.update({key_val[0]:key_val[1]})
+            else:
+                print("{} doesn't have value".format(key_val[0]))
+        else:
+            print (data_dict)
+            camera_test()
+            exit()
+    
